e01f7fb659cff14c76cc9e3d02a9aeb71ba5a3bd
bala-amypo <bala@amypo.in>
2025-12-23 04:11:41 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CreditCardServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CreditCardServiceImpl.java
@@ -1,4 +1,3 @@
-// src/main/java/com/example/demo/service/impl/CreditCardServiceImpl.java
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.CreditCardRecord;
@@ -6,9 +5,11 @@ import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.CreditCardRecordRepository;
 import com.example.demo.service.CreditCardService;
+import org.springframework.stereotype.Service;   
 
 import java.util.List;
 
+@Service  
 public class CreditCardServiceImpl implements CreditCardService {
 
     private final CreditCardRecordRepository cardRepo;
